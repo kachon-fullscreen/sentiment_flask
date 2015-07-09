c1eb67266f91676a6d8c424c6637e83bcfac4c7c
kachon lei <kachon.lei@fullscreen.net>
2015-07-09 00:05:04 -0700

    1.  Add scikit-learn

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
+from sklearn.datasets import fetch_20newsgroups
 
 app = Flask(__name__)
 
@@ -7,3 +8,13 @@ app = Flask(__name__)
 def hello():
     return 'Hello World!'
 
+@app.route('/test')
+def test():
+    categories = ['alt.atheism', 'soc.religion.christian',
+                  'comp.graphics', 'sci.med']
+    twenty_train = fetch_20newsgroups(subset='train',
+                    categories=categories, shuffle=True, random_state=42)
+    return jsonify(results=twenty_train.target_names)
+
+if __name__ == '__main__':
+    app.run(debug=True)
